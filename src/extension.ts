7a62f768e4f52f567acf8ba07c0b2122f3a0bfa5
Paul Sohns <paul.sohns@innogames.com>
2022-02-25 18:43:38 +0100

    Add notes and get shellCommands to work

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,18 @@
 import { stringify } from 'querystring';
 import * as vscode from 'vscode';
 import { resourceLimits } from 'worker_threads';
+import * as cp from 'child_process';
 
 let gitStatusBarItem: vscode.StatusBarItem;
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('innoVS-gitInfo is now active');
 
+	getGitInfo();
+
 	const getInfoCommand = 'sample.showSelectionCount';
 	context.subscriptions.push(vscode.commands.registerCommand(getInfoCommand, () => {
-		vscode.window.showInformationMessage(getCurrentLine(), {modal: true});
+		vscode.window.showInformationMessage(`Line: ${getCurrentLine()}`);
 	}));
 
 	gitStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
@@ -27,19 +30,60 @@ export function activate(context: vscode.ExtensionContext) {
     }
 }
 
-function getCurrentLine(): string {
-	let result: string;
+function getCurrentLine(): number {
+	let result: number;
 
 	if (vscode.window.activeTextEditor) {
-		const n = vscode.window.activeTextEditor.selection.active.line + 1;
-		result = `Line: ${n}`;
+		result = vscode.window.activeTextEditor.selection.active.line + 1;
 	}
 	else {
-		result = `No line \n selected!`;
+		return 0;
 	}
 
 	return result;
 }
+
+async function getGitInfo(): Promise<void> {
+
+	let result = await execShell('pwd', {cwd: '/Volumes/workspace'});
+
+	console.log(result);
+
+	// let execution: ChildProcess;
+
+	// let command = "git";
+	// let args = ["ls-remote", "--get-url"];
+	// let options: ExecOptions = {};
+
+    // try {
+    //     execution = execFile(command, args, { ...options, encoding: "utf8" });
+	// 	console.log(execution);
+    // } catch (err) {
+    //     console.log(err);
+	// 	return;
+    // }
+
+	// let data = '';
+
+	// for await (const chunk of execution?.stdout ?? []) {
+    //     data += chunk;
+	// 	console.log('hello');
+    // }
+
+	// console.log(data);
+}
+
+const execShell = (
+	cmd: string,
+	options: cp.ExecOptions = {} ) =>
+    new Promise<string>((resolve, reject) => {
+        cp.exec(cmd, {...options, encoding: "utf8"}, (err, out) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(out);
+        });
+    });
 
 function updateStatusBarItem(): void {
 	gitStatusBarItem.show();
